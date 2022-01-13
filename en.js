8e3f5367f0d75cde8f07e4bd2dff51202a7844aa
garethbilaney <gareth.bilaney@hhu.de>
2022-01-13 12:16:42 +0100

    Update en.js

--- en.js
+++ en.js
@@ -1,6 +1,25 @@
 export const english = {
-  test: "test",
-  toast: {
-    output: "output",
+  "Please, enter valid credentials.": "Please, enter valid credentials.",
+
+  "login-template": {
+    name: "Login template",
+    "label-extension-heading": "Install the chrome extension.",
+    "label-extension-heading-description":
+      "Add websites and articles with one click to your project!",
+    "button-link-to-extension": "To the chrome store",
+  },
+  "legal-notice": "Copyright © CiteDrive ${year}.",
+  login: {
+    name: "Login",
+    "label-log-in-to-citedrive": "Log in to CiteDrive",
+    "label-email": "Email",
+    "label-password": "Password",
+    "label-warning-email": "Invalid email",
+    "label-warning-required": "Required",
+    "placeholder-email": "",
+    "placeholder-password": "",
+    "link-sign-up-an-account": "Sign up for an account",
+    "link-forgot-password": "Forgot password?",
+    "button-sign-in": "Sign In",
   },
 };

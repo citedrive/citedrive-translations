50f834919d624c23d986f283a0c1181f025f9e61
garethbilaney <gareth.bilaney@hhu.de>
2022-01-14 14:45:45 +0100

    Update en.js

--- en.js
+++ en.js
@@ -1,5 +1,10 @@
 export const english = {
-  "Please, enter valid credentials.": "Please, enter valid credentials!",
+  
+  "Please, enter valid credentials.": "Please, enter valid credentials.",
+  "A user with that username already exists.":
+    "A user with that username already exists.",
+  "A Confirmation Email has been sent to your email.":
+    "A Confirmation Email has been sent to your email.",
 
   "login-template": {
     name: "Login template",
@@ -16,10 +21,17 @@ export const english = {
     "label-password": "Password",
     "label-warning-email": "Invalid email",
     "label-warning-required": "Required",
-    "placeholder-email": "",
-    "placeholder-password": "",
     "link-sign-up-an-account": "Sign up for an account",
     "link-forgot-password": "Forgot password?",
     "button-sign-in": "Sign In",
+  },
+
+  createAccount: {
+    name: "Create Account",
+    "label-create-your-account": "Create your Account",
+    "label-email": "Email",
+    "label-password": "Password",
+    "link-to-login": "Already have an account? Log In",
+    "button-sign-up": "Sign Up",
   },
 };
